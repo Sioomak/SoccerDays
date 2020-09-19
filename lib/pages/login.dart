f08c48c66e7d4472ce8c0af38d3ffd1761157ff9
Siamak Salimi <ssalimi1220@gmail.com>
2020-09-19 17:30:54 -0400

    Opacity added to email, password and login button

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -23,36 +23,39 @@ class _LoginScreenState extends State<LoginScreen> {
           style: kLabelStyle,
         ),
         SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 70.0,
-          child: TextFormField(
-            validator: (String value) {
-              if (value.isEmpty) {
-                return "Please enter your email";
-              } else if (value.length <= 5) {
-                return "Email should be at least 6 characters";
-              }
-              return null;
-            },
-            onSaved: (String value) {
-              _email = value;
-            },
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.email,
+        Opacity(
+          opacity: 0.8,
+          child: Container(
+            alignment: Alignment.centerLeft,
+            decoration: kBoxDecorationStyle,
+            height: 70.0,
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Please enter your email";
+                } else if (value.length <= 5) {
+                  return "Email should be at least 6 characters";
+                }
+                return null;
+              },
+              onSaved: (String value) {
+                _email = value;
+              },
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(
                 color: Colors.white,
+                fontFamily: 'OpenSans',
               ),
-              hintText: 'Enter your Email',
-              hintStyle: kHintTextStyle,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                hintText: 'Enter your Email',
+                hintStyle: kHintTextStyle,
+              ),
             ),
           ),
         ),
@@ -69,36 +72,39 @@ class _LoginScreenState extends State<LoginScreen> {
           style: kLabelStyle,
         ),
         SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 70.0,
-          child: TextFormField(
-            validator: (String value) {
-              if (value.isEmpty) {
-                return "Please enter your password";
-              } else if (value.length <= 5) {
-                return "Password should be at least 6 characters";
-              }
-              return null;
-            },
-            onSaved: (String value) {
-              _password = value;
-            },
-            obscureText: true,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.lock,
+        Opacity(
+          opacity: 0.8,
+          child: Container(
+            alignment: Alignment.centerLeft,
+            decoration: kBoxDecorationStyle,
+            height: 70.0,
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return "Please enter your password";
+                } else if (value.length <= 5) {
+                  return "Password should be at least 6 characters";
+                }
+                return null;
+              },
+              onSaved: (String value) {
+                _password = value;
+              },
+              obscureText: true,
+              style: TextStyle(
                 color: Colors.white,
+                fontFamily: 'OpenSans',
               ),
-              hintText: 'Enter your Password',
-              hintStyle: kHintTextStyle,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.white,
+                ),
+                hintText: 'Enter your Password',
+                hintStyle: kHintTextStyle,
+              ),
             ),
           ),
         ),
@@ -151,27 +157,30 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 25.0),
       width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () {
-          if (!_formKey.currentState.validate()) {
-            return;
-          }
-          _formKey.currentState.save();
-        },
-        padding: EdgeInsets.all(15.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        color: Colors.white,
-        child: Text(
-          'LOG IN',
-          style: TextStyle(
-            color: Color(0xFF3b5249),
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
+      child: Opacity(
+        opacity: 0.7,
+        child: RaisedButton(
+          elevation: 5.0,
+          onPressed: () {
+            if (!_formKey.currentState.validate()) {
+              return;
+            }
+            _formKey.currentState.save();
+          },
+          padding: EdgeInsets.all(15.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          color: Colors.white,
+          child: Text(
+            'LOG IN',
+            style: TextStyle(
+              color: Color(0xFF3b5249),
+              letterSpacing: 1.5,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'OpenSans',
+            ),
           ),
         ),
       ),
@@ -305,12 +314,17 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(50.0),
-                child: Image.asset(
-                  'assets/logos/ClipartKey_56184.png',
-                  width: 70,
-                  height: 70,
+              Opacity(
+                opacity: 0.5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/logos/ClipartKey_56184.png',
+                      width: 500,
+                      height: 500,
+                    ),
+                  ],
                 ),
               ),
               Container(
